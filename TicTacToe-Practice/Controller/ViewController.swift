a959a34477276bfe937db967840dfe504936eaa9
Guy Thomlinson <thomlinsong001@mymail.mssu.edu>
2021-04-08 12:21:57 -0700

    Fixed the code, Added images to assert library, Finalized requirements (i think), also made it so that you can see how long the game will take to reset because that sounds like a nice feature :)

--- TicTacToe-Practice/Controller/ViewController.swift
+++ TicTacToe-Practice/Controller/ViewController.swift
@@ -28,42 +28,62 @@ class ViewController: UIViewController {
     // Was done to start, but doubled checked!
     
     //TODO (in the View, and link here in the Controller): Create a new label for "status text" Ex: Whose turn it is, whether someone won, and whether there was an invalid move
+    @IBOutlet weak var resetTimerLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
-    
+   
+    func initializeStatus(){
+        statusLabel.text = "\(ticTacToe.getTurn())'s Turn!"
+    }
     @IBAction func buttonClicked(_ sender: UIButton) {
         //Step 1 - Get the row number and column number from the button text (See hint in assignment instructions)
         //get Row Number
         let indexRow = 2
+        
         let charAtRow = (sender.titleLabel?.text?.index((sender.titleLabel?.text!.startIndex)!, offsetBy: indexRow))!
-        let Row = sender.titleLabel?.text?[charAtRow].wholeNumberValue! ?? -1
+        let row = sender.titleLabel?.text?[charAtRow].wholeNumberValue! ?? -1
         
         //get Col Number
         let indexCol = 0
         let charAt = (sender.titleLabel?.text?.index((sender.titleLabel?.text!.startIndex)!, offsetBy: indexCol))!
-        let Col = sender.titleLabel?.text?[charAt].wholeNumberValue! ?? -1
+        let col = sender.titleLabel?.text?[charAt].wholeNumberValue! ?? -1
         
         //Step 2 - Place the marker in the *model*
         
-        if (Row >= 0 && Row <= 2) && (Col >= 0 && Col <= 2){
+        if (ticTacToe.placeMarker(row: row, column: col)){
         //If placing was successful....
            //Step 3 - Get the turn from the *model* and place the appropriate image in the spot in the images array
-            images?[Row][Col].image = UIImage(named: "\(ticTacToe.getTurn()).png")
+            images![col][row].image = UIImage(named: ticTacToe.getTurn())
            //Step 4 - Check for a winner, updating the status message and creating a countdown timer to reset the game if necessary
         if (ticTacToe.didWin() != " "){
             
             statusLabel.text = "\(ticTacToe.didWin()) Won!"
-           
-            for row in 0...2{
-                for col in 0...2{
-                    images?[row][col].image = UIImage(named: "null.png")
+           var resetTime = 15
+            self.resetTimerLabel.text =  "Resetting game in \(resetTime) seconds"
+            Timer.scheduledTimer(withTimeInterval:1.0,repeats: true){timer in
+             resetTime -= 1
+                self.resetTimerLabel.text =  "Resetting game in \(resetTime) seconds"
+                if resetTime == 0{
+                    self.ticTacToe.reset()
+                    self.resetTimerLabel.text = ""
+                    self.statusLabel.text = "\(self.ticTacToe.getTurn())'s Turn!"
+                    for row in 0...2{
+                        for col in 0...2{
+                            self.images![row][col].image = UIImage(named: "blank")
+                }
+            }
+                    timer.invalidate()
                 }
             }
         }
            // Check the EggTimer assignment on Udemy for hints on creating the countdown timer
         
            //Step 5 - If no one won, make the *model* go to the next turn, get the current turn from the model and update the status message.
-        ticTacToe.nextTurn()
+            
+        else {
+            ticTacToe.nextTurn()
         statusLabel.text = "\(ticTacToe.getTurn())'s Turn!"
+            }
+            
     }
         //If placing the marker wasn't successful, change the status message, so the user knows what's wrong
         else{
@@ -78,7 +98,7 @@ class ViewController: UIViewController {
         images = [[upperLeft, upperMiddle, upperRight],
                   [centerLeft, centerMiddle, centerRight],
                   [bottomLeft, bottomMiddle, bottomRight]]
-
+        initializeStatus()
     }
 
 
